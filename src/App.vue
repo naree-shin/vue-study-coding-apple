c40310bb6b31a1fd6b5a9c59ab11fcc88a1638c9
shin Naree <nr.rad00@gmail.com>
2022-04-04 10:08:36 +0900

    study : 조건식 : 모달창만들기

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
+
+    <!-- 모달팝업 -->
+    <div class="black-bg" v-if="모달창 == true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지 내용</p>
+        <button @click="모달창 = false">닫기</button>
+      </div>
+    </div>
+
     <!-- 메뉴 -->
     <div class="menu">
       <a v-for="a in menuArray" :key="a">{{a}}</a>
     </div>
 
     <div>
-      <!-- 신고수버튼 여러개 만들기-->
-      
       <img src="./assets/images/room0.jpg" class="room-img">
-      <h4>{{shop[0]}} 원룸</h4>
+      <h4 @click="모달창 = true">{{shop[0]}} 원룸</h4>
       <p>50 만원</p>
       <button @click="신고수[0] += 1">허위매물신고</button>
       <span>신고수 : {{신고수[0]}}</span>
@@ -29,7 +37,6 @@
       <span>신고수 : {{신고수[2]}}</span>
     </div>
 
-
   </div>
 </template>
 
@@ -39,6 +46,7 @@ export default {
   name: 'App',
   data(){
     return {
+      모달창 : false,
       신고수 : [0,0,0],
       menuArray : ['Home','Shop','About'],
       shop : ['역삼동','천호동','마포구']
@@ -56,6 +64,26 @@ export default {
 </script>
 
 <style>
+/* 모달팝업 */
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -69,4 +97,7 @@ export default {
   width: 100%;
   margin-top: 40px;
 }
+
+
+
 </style>
