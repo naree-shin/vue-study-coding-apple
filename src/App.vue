b4e9476be63f7b7be43f0bedaaed817226affdaa
shin Naree <nr.rad00@gmail.com>
2022-04-04 09:03:50 +0900

    study : 이미지 링크달기

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,28 @@
 
     <div>
       <!-- 신고수버튼 여러개 만들기-->
+      
+      <img src="./assets/images/room0.jpg" class="room-img">
       <h4>{{shop[0]}} 원룸</h4>
       <p>50 만원</p>
       <button @click="신고수[0] += 1">허위매물신고</button>
       <span>신고수 : {{신고수[0]}}</span>
     </div>
     <div>
+      <img src="./assets/images/room1.jpg" class="room-img">
       <h4>{{shop[1]}} 원룸</h4>
       <p>50 만원</p>
       <button @click="신고수[1] += 1">허위매물신고</button>
       <span>신고수 : {{신고수[1]}}</span>
     </div>
     <div>
+      <img src="./assets/images/room2.jpg" class="room-img">
       <h4>{{shop[2]}} 원룸</h4>
       <p>50 만원</p>
       <button @click="신고수[2] += 1">허위매물신고</button>
       <span>신고수 : {{신고수[2]}}</span>
     </div>
+
 
   </div>
 </template>
@@ -59,5 +64,9 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
