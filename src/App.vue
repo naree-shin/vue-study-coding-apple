c58712c206952ffeeefb468e88d68629f837a89b
naree-shin <nr.shin000@gmail.com>
2022-09-05 13:25:35 +0900

    change modal-pop to component, add discount-banner

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div>
 
-    <!-- 모달팝업 -->
-    <div class="black-bg" v-if="모달창 == true">
-      <div class="white-bg">
-        <h4>상세페이지</h4>
-        <p>상세페이지 내용</p>
-        <button @click="모달창 = false">닫기</button>
-      </div>
-    </div>
+    <modal-pop></modal-pop>
 
     <!-- 메뉴 -->
     <div class="menu">
       <a v-for="a in menuArray" :key="a">{{a}}</a>
     </div>
 
-    <div>
+    <discount-banner></discount-banner>
+
+    <div v-for="(a, i) in 원룸들" :key="i">
       <!-- 속성안에 데이터바인딩 속성앞에 : 를 붙여야함 -->
-      <img :src="원룸들[0].image" class="room-img">
-      <h4>{{원룸들[0].title}} 원룸</h4>
-      <p>{{원룸들[0].price}}원</p>
+      <img :src="a.image" class="room-img">
+      <h4 @click="모달창 == true; 누른거 = i">{{a.title}} 원룸</h4>
+      <p>{{a.price}}원</p>
     </div>
 
   </div>
@@ -28,12 +23,15 @@
 <script>
 // oneroom.js -> export default apple
 import data01 from './assets/js/data/oneroom.js';
+import DiscountBanner from './components/DisconutBanner';
+import ModalPop from './components/ModalPop';
 
 export default {
   name: 'App',
   data(){
     return {
       원룸들 : data01,
+      누른거: 0,
       모달창 : false,
       신고수 : [0,0,0],
       menuArray : ['Home','Shop','About'],
@@ -47,6 +45,8 @@ export default {
     }
   },
   components: {
+    DiscountBanner,
+    ModalPop,
   }
 }
 </script>
---
+++ src/components/ModalPop.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 모달팝업 -->
+    <div class="black-bg" v-if="모달창 == true">
+      <div class="white-bg">
+        <img :src="원룸들[누른거].image" style="width:100%">
+        <h4>{{원룸들[누른거].title}}</h4>
+        <p>{{원룸들[누른거].content}}</p>
+        <p>{{원룸들[누른거].price}} 원</p>
+        <button @click="모달창 = false">닫기</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalPop',
+}
+</script>
+
+<style>
+
+</style>
